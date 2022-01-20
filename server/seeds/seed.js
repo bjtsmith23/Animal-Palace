5c9a7209f614c924a269121098be672b9d549e16
Brian Smith <bjtsmith23@gmail.com>
2022-01-20 11:17:43 -0500

    added seeds and json data

---
+++ server/seeds/seed.js
@@ -0,0 +1,4 @@
+const db = require('../config/connection');
+const { User, Animal } = require('../models');
+const userData = require('./userData.json');
+const animalData = require('./animalData.json');
