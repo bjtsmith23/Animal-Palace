b0dcb4981a9e6a24d356ba6a29a0ab3fa91206dd
Marc Negron <negronmarc@gmail.com>
2022-01-20 14:34:09 -0500

    Delete server directory

--- server/seeds/seed.js
+++
@@ -1,4 +0,0 @@
-const db = require('../config/connection');
-const { User, Animal } = require('../models');
-const userData = require('./userData.json');
-const animalData = require('./animalData.json');
