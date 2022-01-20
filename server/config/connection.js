079e7431b436d9e99b6c2a8bf384f695fa2bd096
Brian Smith <bjtsmith23@gmail.com>
2022-01-20 14:42:15 -0500

    edited seeds and connection data

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/donation-app', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/donation-app-db', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
