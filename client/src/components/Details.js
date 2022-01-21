d7961e52f9a42b3207aaa4b7bec249a9293f0890
bchen41 <bettychen41@outlook.com>
2022-01-20 21:28:59 -0500

    refactored code and added css

--- client/src/components/Details.js
+++ client/src/components/Details.js
@@ -1,6 +1,6 @@
 import React from "react";
+import "../assets/css/Details.css";
 
-// import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
@@ -11,27 +11,33 @@ import HomePuppy from "../assets/images/home-puppy.jpg";
 
 export default function Details() {
   return (
-    <div>
+    <Card className="border-0">
       <Row>
         <Col>
-          <Card.Img variant="top" src={HomePuppy} />
+          <Card.Img src={HomePuppy} className="detail-img" />
         </Col>
-        <Col>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
 
-          <ListGroup className="list-group-flush">
-            <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-            <ListGroupItem>Vestibulum at eros</ListGroupItem>
-            <ListGroupItem>Vestibulum at eros</ListGroupItem>
-          </ListGroup>
-          <Card.Body>
+        <Col>
+          <Card>
+            <Card.Body>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card>
+            <ListGroup className="list-group-flush">
+              <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+              <ListGroupItem>Vestibulum at eros</ListGroupItem>
+              <ListGroupItem>Vestibulum at eros</ListGroupItem>
+            </ListGroup>
+          </Card>
+          <Card.Body className="text-center">
             <Card.Link href="#">Add to Cart</Card.Link>
           </Card.Body>
         </Col>
       </Row>
-    </div>
+    </Card>
   );
 }
