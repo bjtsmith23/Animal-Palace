d9c83160d6413a40d60ea7716a940aa789f0b6c1
bchen41 <bettychen41@outlook.com>
2022-01-21 19:56:59 -0500

    removed hard coded data, imported useQuery to fetch backend data, refactored code to reflect typeDefs

--- client/src/components/MainCard.js
+++ client/src/components/MainCard.js
@@ -5,61 +5,53 @@ import ListGroup from "react-bootstrap/ListGroup";
 import ListGroupItem from "react-bootstrap/ListGroupItem";
 import Button from "react-bootstrap/Button";
 
-import exampleDog from "../assets/images/home-puppy.jpg";
-import exampleCat from "../assets/images/home-cat.jpg";
+import { useQuery } from "@apollo/client";
+import { QUERY_ALL_ANIMALS } from "../utils/queries";
 
 export default function MainCard() {
-  const mainContent = [
-    {
-      _id: "ljlkasjera",
-      cardImg: exampleDog,
-      cardTitle: "SPOTS",
-      cardSex: "F",
-      cardAge: 1,
-      cardDescription: "abcdefg",
-    },
-    {
-      _id: "laskjfalskjera",
-      cardImg: exampleCat,
-      cardTitle: "SPOTS2",
-      cardSex: "M",
-      cardAge: 2,
-      cardDescription: "hijklmnop",
-    },
-  ];
+  const { loading, data } = useQuery(QUERY_ALL_ANIMALS);
+  const animals = data?.animals || [];
+
+  console.log(animals);
+
   const [selectedModal, setSelectedModal] = useState();
   const [modalShow, setModalShow] = useState(false);
   const handleOnclick = (e) => {
     setModalShow(!modalShow);
     const id = e.target.getAttribute("data-modal-id");
-    const matchingContent = mainContent.filter((mc) => mc._id === id)[0];
+    const matchingContent = animals.filter((animal) => animal._id === id)[0];
     setSelectedModal(matchingContent);
   };
   return (
     <>
-      {mainContent.map((content, index) => (
-        <div className="card-container m-5 border-0 shadow" key={index}>
-          <Card style={{ width: "18rem" }}>
-            <Card.Img variant="top" src={content.cardImg} />
-            <Card.Body>
-              <Card.Title>{content.cardTitle}</Card.Title>
-            </Card.Body>
-            <ListGroup className="list-group-flush">
-              <ListGroupItem>Sex: {content.cardSex} </ListGroupItem>
-              <ListGroupItem>Age: {content.cardAge} </ListGroupItem>
-            </ListGroup>
-            <Card.Body>
-              <Button
-                variant="primary"
-                data-modal-id={content._id}
-                onClick={handleOnclick}
-              >
-                Learn More!
-              </Button>
-            </Card.Body>
-          </Card>
-        </div>
-      ))}
+      {loading ? <div>Loading...</div> : null}
+
+      {animals
+        ? animals.map((animal, index) => (
+            <div className="card-container m-5 border-0 shadow" key={index}>
+              <Card style={{ width: "18rem" }}>
+                <Card.Img variant="top" src={animal.image} />
+                <Card.Body>
+                  <Card.Title>{animal.name}</Card.Title>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                  <ListGroupItem>Sex: {animal.sex} </ListGroupItem>
+                  <ListGroupItem>Age: {animal.age} </ListGroupItem>
+                </ListGroup>
+                <Card.Body>
+                  <Button
+                    variant="primary"
+                    data-modal-id={animal._id}
+                    onClick={handleOnclick}
+                  >
+                    Learn More!
+                  </Button>
+                </Card.Body>
+              </Card>
+            </div>
+          ))
+        : null}
+
       <MainCardModal
         show={modalShow}
         modalinfo={selectedModal}
