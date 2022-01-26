c1efce6b52ba8879c06d839280a782ee63b1fa2e
bchen41 <bettychen41@outlook.com>
2022-01-25 20:04:05 -0500

    reafactored to open DonateModal instead of leading to stripe checkout

--- client/src/pages/Donate.js
+++ client/src/pages/Donate.js
@@ -1,35 +1,18 @@
-import React, { useEffect } from "react";
-import Nav from "react-bootstrap/Nav";
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import DonateModal from "../components/DonateModal";
 
-import { useLazyQuery } from "@apollo/client";
-import { QUERY_CHECKOUT } from "../utils/queries";
-import { loadStripe } from "@stripe/stripe-js";
-const stripePromise = loadStripe(
-  "pk_test_51KLThrKTqyKGrYgVVrsKQTTMxSDlTqtuBSF9JcxrJzKFlwOHVKU2BteNUBq1m1cw0wPE3gfCIZXNHMOJzKJUnjjk00xacvbzIb"
-);
-
-export default function CheckoutForm({ children, donation }) {
-  const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT, {
-    variables: { initialDonation: donation },
-  });
-
-  useEffect(() => {
-    if (data) {
-      console.log(data);
-      stripePromise.then((res) => {
-        res.redirectToCheckout({ sessionId: data.checkout.session });
-      });
-    }
-  }, [data]);
-
-  const handleSubmit = async (ev) => {
-    ev.preventDefault();
-    getCheckout();
-  };
+export default function CheckoutForm() {
+  // modal state for donate button
+  const [donateModalShow, setDonateModalShow] = useState(false);
 
   return (
     <div>
-      <Nav.Link onClick={handleSubmit}>{children}</Nav.Link>
+      <DonateModal
+        show={donateModalShow}
+        onHide={() => setDonateModalShow(false)}
+      />
+      <Button onClick={() => setDonateModalShow(true)}>Donate</Button>
     </div>
   );
 }
